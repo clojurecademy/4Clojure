f5caa9eb04eb5dd89afc3d1a31a5895215a2b76e
Ertugrul Cetin <ertugrulcetin@Ertugruls-MBP.fritz.box>
2017-09-30 16:28:59 +0200

    update plugin version

--- project.clj
+++ project.clj
@@ -10,7 +10,7 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clojurecademy "0.1.0"]]
 
-  :plugins [[lein-clojurecademy "0.1.5"]]
+  :plugins [[lein-clojurecademy "0.1.55"]]
 
   :eval-in :leiningen
 
