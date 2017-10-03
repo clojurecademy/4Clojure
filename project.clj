49e3f822e41158243b10e339e2b393650d7fdc55
Ertugrul Cetin <ertugrulcetin@Ertugruls-MacBook-Pro.local>
2017-10-03 13:56:06 +0200

    license added

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
 
   :url "http://example.com/FIXME"
 
-  :license {:name "Eclipse Public License"
-            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "MIT License"
+            :url  "https://opensource.org/licenses/MIT"}
 
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clojurecademy "0.1.0"]]
