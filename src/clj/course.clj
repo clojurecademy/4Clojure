2062b3300b75bb86c35fa43ffafa79dd9cb8f4c2
Ertugrul Cetin <ertugrulcetin@Ertugruls-MacBook-Pro.local>
2017-10-05 22:08:28 +0200

    typo fix

--- src/clj/course.clj
+++ src/clj/course.clj
@@ -8,7 +8,7 @@
                            :report-bug-email-or-link "https://github.com/clojurecademy/4Clojure/issues"
                            :name 'for-clojure
                            :skip? true
-                           :short-description "4Clojure problems with few differences"
+                           :short-description "4Clojure problems with a few differences"
                            :long-description "4Clojure(4clojure.com) is a resource to help fledgling Clojurians learn the language through interactive problems.
                         The first few problems are easy enough that even someone with no prior experience should find the learning curve forgiving.
                         Also, in this course content of http://clojure-doc.org has been used for teaching Clojure basics."
